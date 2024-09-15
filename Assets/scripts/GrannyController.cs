2fb7e3215f4c24d71db40b7b364b74dddf766ddc
Lexie Zhang <zay20040811@gmail.com>
2024-09-15 05:36:28 -0400

    update unity scripts

--- Assets/scripts/GrannyController.cs
+++ Assets/scripts/GrannyController.cs
@@ -1,16 +1,33 @@
-using System;
 using UnityEngine;
-using System.Collections.Generic;
 
-public class AnimationController : MonoBehaviour
+public class GrannyController : MonoBehaviour
 {
     private Animator animator;
+
+    // Initialize the animator component
+    void Start()
+    {
+        // This assumes that the Animator component is attached to the same GameObject (SportyGranny).
+        animator = GetComponent<Animator>();
+
+        // If for some reason the Animator is not found, log an error.
+        if (animator == null)
+        {
+            Debug.LogError("Animator component not found on the GameObject.");
+        }
+    }
 
     // Method to trigger animation based on an event
     public void ChangeAnimation(string parameter)
     {
-        Debug.Log("ChangeAnimation called from JavaScript");
-        //prameter will be the trigger name in unity
-        animator.SetTrigger(parameter); 
+        if (animator != null)
+        {
+            Debug.Log("ChangeAnimation called from JavaScript with parameter: " + parameter);
+            animator.SetTrigger(parameter);
+        }
+        else
+        {
+            Debug.LogError("Animator is not assigned or found.");
+        }
     }
 }
